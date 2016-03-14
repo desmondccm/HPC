2d39827887b77246589bdc3e1efbc2fdb8d9e2ce
desmondccm <dcheung112@gmail.com>
2016-03-14 14:57:21 +0000

    Variables are defined in this version of the code as global variables

--- HPC_Q1/HPC_Q1/main.cpp
+++ HPC_Q1/HPC_Q1/main.cpp
@@ -7,8 +7,26 @@
 //
 
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+int main() {
+    cout << "this codes HPC code 1 in MATLAB. The final Vector containing the heat distribution of the bar is displayed" << endl;
+    cout << "Timestep is 0.001s, run time is 10s, alpha = 1, bar end temperatures = 0 K and ";
+    
+    //Declare variables used for the problem
+    double alpha = 1; //heat conductivity
+    double dt = 0.001; //time step size
+    int Nx = 20; //space steps
+    double L = 1; //bar length
+    double gamma0 = 0; //temperature at front end of bar (BC1)
+    double gamma1 = 0; //temeprature at rear end of bar (BC2)
+    double T = 10; //run time
+    double Nt = T/dt; //number of time steps
+    double dx = L/Nx;
+    double nu = alpha*dt/dx/dx;
+    
+    
+    
     return 0;
 }
