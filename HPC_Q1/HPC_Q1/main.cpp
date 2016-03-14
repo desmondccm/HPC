881cd86527435b24f93d314aebf5f8549c2a6c87
desmondccm <dcheung112@gmail.com>
2016-03-14 13:16:08 +0000

    The Beginning
    Programme starting code, with default Xcode hello world built in

---
+++ HPC_Q1/HPC_Q1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HPC_Q1
+//
+//  Created by Desmond Cheung on 14/03/2016.
+//  Copyright (c) 2016 cmc213. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
