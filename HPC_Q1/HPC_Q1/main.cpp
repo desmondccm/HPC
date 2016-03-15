31d26d70882ff872fae3951230a4e2c6672c5c9b
desmondccm <dcheung112@gmail.com>
2016-03-15 03:21:36 +0000

    This is un updated class definition that includes a matrix multiplication and matrix generation within the class definition of TriMatrix. However it is not sure whether it is successful.

--- HPC_Q1/HPC_Q1/main.cpp
+++ HPC_Q1/HPC_Q1/main.cpp
@@ -7,16 +7,49 @@
 //
 
 #include <iostream>
-#include <math.h>
 #include <vector>
 using namespace std;
+typedef vector<double> Row;         //declare a single row
+typedef vector<Row> Matrix;         //Define a matrix as a colum of rows
 
-class TriM                          //declare a Tri-Matrix class used for the multiplication
+class TriMatrix                     //declare a Tri-Matrix class, a special type of matrix that consist of a non-zero diagonal, upper diagonal and lower diagonal.
 {
-    public:
+public:
+    int dim;
+    double diag, updiag, lowdiag;   //used for matrix generation and matrix properties
+    vector<double> vectorin;        //used for matrix multiplication.
+
+    Matrix Matrixgen(void)          //define a function that generates a matrix
+    {
+        Matrix M(dim, Row(dim));
+        for (int i=0; i<dim; i++) {
+            M[i][i]=diag;           //filling middle diagonal
+            M[i][i+1]=updiag;       //filling upper diagonal
+            M[i][i-1]=lowdiag;      //filling lower diagonal
+        }
+        M[0][0]=1;                  //middle diagonal always starts 1
+        M[dim][dim]=1;              //middle diagonal always ends with 1
+        M[dim][dim-1]=0;            //lower diagonal always ends with 0
+        M[1][2]=0;                  //upper diagonal always starts with 0
+        return M;
+    }
+    
+    vector<double> Matrixmult(void)         //defines a function that multiplies matrices to vectors
+    {
+        Matrix A = Matrixgen();     //Calls the matrix generation function to generate a Matrix
+        vector<double> vectorout(dim); //initialize an output vector
+        double sum = 0;             //initilizes a sum variable representing each row
+        for (int i=0; i<dim; i++) { //defines a forloop that writes member to vector
+            for (int j=0; j<dim; j++) {
+                double dum=A[i][j]*vectorin[j];
+                sum=dum+sum;
+            }
+            vectorout[i]=sum;
+        }
+    return vectorout;
+    }
+
 };
-
-
 
 int main() {
     /*----------------------------- programme description: -------------------------------------------------------------------------------------*/
@@ -41,23 +74,20 @@ int main() {
     vector<double> u0((Nx+1));      //initial heat distribution
     vector<double> u1((Nx+1));      //vector defined for matrix multiplication
     vector<double> u2((Nx+1));      //vector defined for receipient of matrix multiplcation
-    typedef vector<int> Row;        //declare a single row
-    typedef vector<Row> Matrix;     //Define a matrix as a colum of rows
-    Matrix M((Nx+1),Row(Nx+1));
     
 
     /*----------------------------- Defining the x-co-ordinate space by creating an x-vector -----------------------------------------------------*/
     
     for (int i=0; i<Nx+1; i++) {    //fill vector based on the size of size of the vector
-        x[i]=0+i*dx;    //increments the x vector according to dx size
+        x[i]=i*dx;    //increments the x vector according to dx size
         //cout<<x[i]<<endl; //check that the x-vector makes sense
     }
     
     /*----------------------------- Defining the u0 vector as the initial heat distribution along the bar ----------------------------------------*/
     
     for (int i=0; i<Nx+1; i++) {    //fill vector based on the size of size of the vector
-        u0[i]=x[i]/(1-x[i]);    //increments the x vector according to dx size
-        //cout<<u0[i]<<endl; //check that the x-vector makes sense
+        u0[i]=x[i]/(1-x[i]);        //increments the x vector according to dx size
+        //cout<<u0[i]<<endl;        //check that the x-vector makes sense
     }
     
     /*----------------------------- constructing the starting initial u vector for the heat distribution ------------------------------------------*/
@@ -70,16 +100,21 @@ int main() {
     }
                                     //cout<<u1[Nx+1]<<endl;
     
-    /*----------------------------- Construct the Tri-diagonal Matrix M ---------------------------------------------------------------------------*/
-    for (int i=0; i<Nx+1; i++) {
-        M[i][i]=1-2*nu;             //filling middle diagonal
-        M[i][i+1]=nu;               //filling upper diagonal
-        M[i][i-1]=nu;               //filling loer diagonal
+    /*------------------------ Construct the Tri-diagonal Matrix Mat by defining it as an object within class TriMatrix ---------------------------*/
+    TriMatrix Mat;
+    Mat.dim = Nx-1;
+    Mat.diag = 1-2*nu;
+    Mat.updiag = nu;
+    Mat.lowdiag = nu;
+    Matrix Matt=Mat.Matrixgen();
+    
+    /*------------------------ Conduct the actual time-integration process as specified by the matrix multiplying method --------------------------*/
+    
+    Mat.vectorin = u1;
+    for (double t=0; t<T; t+=dt) {
+        u2=Mat.Matrixmult();
+        Mat.vectorin = u2;
     }
-    M[0][0]=1;                      //middle diagonal always starts 1
-    M[Nx][Nx]=1;                    //middle diagonal always ends with 1
-    M[Nx][Nx-1]=0;                  //lower diagonal always ends with 0
-    M[1][2]=0;                      //upper diagonal always starts with 0
     
     return 0;
 }
