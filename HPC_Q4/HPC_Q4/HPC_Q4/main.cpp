6a6aecccf643deb87a31538cec75e2c58649be81
desmondccm <dcheung112@gmail.com>
2016-03-19 19:12:14 +0000

    Initial Commit

---
+++ HPC_Q4/HPC_Q4/HPC_Q4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HPC_Q4
+//
+//  Created by Desmond Cheung on 19/03/2016.
+//  Copyright (c) 2016 Imperial College London. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
